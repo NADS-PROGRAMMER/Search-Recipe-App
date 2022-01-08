8fdd55388d65c6d0307f16b68c32eb50f32c4b20
Adrian Nads <marcelonads@gmail.com>
2022-01-08 17:24:51 +0800

    Added a conditional rendering in Section Format.js to ensure that the modal section is not empty

--- src/recipe-info-components/SectionFormat.js
+++ src/recipe-info-components/SectionFormat.js
@@ -2,25 +2,32 @@ import React from 'react'
 
 function SectionFormat({ sectionTitle, list }) {
     return (
-        <div>
-            <section className="flex justify-between items-center">
+        <>
+            { list.length > 0 ? <div>
+                <section className="flex justify-between items-center">
+                    <h3 
+                        className="text-orange-900 font-semibold font-['Poppins'] text-xl">
+                        {sectionTitle}
+                    </h3>
+                </section>
+
+                <ul className="px-5 list-disc">
+                    {
+                        list.map((item, index) => {
+
+                        return ( 
+                            <li className="text-orange-700" key={index}>{item}</li>
+                        )
+                        })
+                    }
+                </ul>
+            </div> : 
                 <h3 
                     className="text-orange-900 font-semibold font-['Poppins'] text-xl">
-                    {sectionTitle}
+                    There are no {sectionTitle} specified.
                 </h3>
-            </section>
-
-            <ul className="px-5 list-disc">
-                {
-                    list.map((item, index) => {
-
-                       return ( 
-                        <li className="text-orange-700" key={index}>{item}</li>
-                       )
-                    })
-                }
-            </ul>
-        </div>
+            }
+        </>
     )
 }
 
