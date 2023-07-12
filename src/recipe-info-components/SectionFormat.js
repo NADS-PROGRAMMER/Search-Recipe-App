6ee5d1fd2c91d906aa8fb151005c6b915309c43c
Adrian Nads <adrian.marcelo.pixel8@gmail.com>
2023-07-12 11:45:45 +0800

    FIXED the error in SectionFormat.js

--- src/recipe-info-components/SectionFormat.js
+++ src/recipe-info-components/SectionFormat.js
@@ -3,9 +3,9 @@ import React from 'react'
 function SectionFormat({ sectionTitle, list }) {
     return (
         <>
-            { list.length > 0 ? <div>
+            {list?.length > 0 ? <div>
                 <section className="flex justify-between items-center">
-                    <h3 
+                    <h3
                         className="text-orange-900 font-semibold font-['Poppins'] text-xl">
                         {sectionTitle}
                     </h3>
@@ -15,14 +15,14 @@ function SectionFormat({ sectionTitle, list }) {
                     {
                         list.map((item, index) => {
 
-                        return ( 
-                            <li className="text-orange-700" key={index}>{item}</li>
-                        )
+                            return (
+                                <li className="text-orange-700" key={index}>{item}</li>
+                            )
                         })
                     }
                 </ul>
-            </div> : 
-                <h3 
+            </div> :
+                <h3
                     className="text-orange-900 font-semibold font-['Poppins'] text-xl">
                     There are no {sectionTitle} specified.
                 </h3>
